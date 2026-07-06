840c2831093dd1b5488b60f3937dfae652f9db29
NatyMulugetaAlemu <metasebiazeleke@gmail.com>
2026-07-05 21:34:29 -0900

    Merging

---
+++ backend/src/server.js
@@ -0,0 +1,27 @@
+import express from "express"
+import "dotenv/config"
+import { initDB} from "./config/db.js"
+import rateLimiter from "../src/middleware/rateLimiter.js"
+import transactionsRoute from "./routes/transactionsRoute.js"
+import job from "./config/cron.js"
+
+const app = express()
+
+app.use(rateLimiter)
+app.use(express.json())
+
+if(process.env.NODE_ENV === "production")job.start()
+
+const PORT = process.env.PORT || 5001
+
+app.get("/api/health", (req, res) => {
+    res.status(200).json({ status: "okay" });
+});
+
+app.use("/api/transactions", transactionsRoute);
+
+initDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on PORT:${PORT}`)
+    })
+}) 
---
+++ mobile/constants/api.js
@@ -0,0 +1,2 @@
+export const API_URL = "https://book-rec-k3ld.onrender.com/api";
+
