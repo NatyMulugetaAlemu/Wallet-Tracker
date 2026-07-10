cc0e2afde499ed32893a20cef217458d9cf0be9f
NatyMulugetaAlemu <metasebiazeleke@gmail.com>
2026-07-09 22:25:07 -0900

    refactor(auth): enhance signup validation and response structure

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -2,55 +2,66 @@ import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 
-
 export const signup = async (req, res) => {
-    const { username, email, password } = req.body;
-    try {
-        if (!username || !email || !password) {
-            return res.status(400).json({ message: "All fields are required" });
-        }
-
-        if (password.length < 6) {
-            return res.status(400).json({ message: "Password must be at least 6 characters" });
-        }
-
-        const existingEmail = await User.findOne({ email });
-        if (existingEmail) {
-            return res.status(400).json({ message: "Email already exists" });
-        }
-
-        const existingUsername = await User.findOne({ username });
-        if (existingUsername) {
-            return res.status(400).json({ message: "Username already exists" });
-        }
-
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-
-        const newUser = new User({
-            username,
-            email,
-            password: hashedPassword,
-        });
-
-        if (newUser) {
-            // generate jwt token here
-            generateToken(newUser._id, res);
-            await newUser.save();
-
-            res.status(201).json({
-                _id: newUser._id,
-                username: newUser.username,
-                email: newUser.email,
-                profilePic: newUser.profilePic,
-            });
-        } else {
-            res.status(400).json({ message: "Invalid user data" });
-        }
-    } catch (error) {
-        console.log("Error in signup controller", error.message);
-        res.status(500).json({ message: "Internal Server Error" });
+  const { username, email, password } = req.body;
+  try {
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
     }
+
+    if (password.length < 6) {
+      return res.status(400).json({ message: "Password must be at least 6 characters" });
+    }
+
+    if (username.length < 3) {
+      return res.status(400).json({ message: "Username should be at least 3 characters long" });
+    }
+
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
+    const existingUsername = await User.findOne({ username });
+    if (existingUsername) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      // generate jwt token here
+      const token = generateToken(newUser._id, res);
+      await newUser.save();
+
+
+      res.status(201).json({
+        token,
+        newUser: {
+          id: newUser._id,
+          username: newUser.username,
+          email: newUser.email,
+          profilePic: newUser.profilePic,
+          createdAt: newUser.createdAt,
+        },
+
+      });
+
+
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
 export const login = async (req, res) => {
@@ -67,13 +78,17 @@ export const login = async (req, res) => {
       return res.status(400).json({ message: "Invalid credentials" });
     }
 
-    generateToken(user._id, res);
+    const token = generateToken(newUser._id, res);
+    res.status(201).json({
+      token,
+      newUser: {
+        id: newUser._id,
+        username: newUser.username,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+        createdAt: newUser.createdAt,
+      },
 
-    res.status(200).json({
-      _id: user._id,
-      username: user.username,
-      email: user.email,
-      profilePic: user.profilePic,
     });
   } catch (error) {
     console.log("Error in login controller", error.message);
@@ -91,8 +106,6 @@ export const logout = (req, res) => {
   }
 };
 
-
-
 export const checkAuth = (req, res) => {
   try {
     res.status(200).json(req.user);
@@ -101,3 +114,5 @@ export const checkAuth = (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+
